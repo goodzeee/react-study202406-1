3b69314d0e25bf79488013e39a7394cbb11f0441
soongu <naong526@naver.com>
2024-06-12 12:24:06 +0900

    # 이벤트 바인딩

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -31,6 +31,16 @@ const ExpenseItem = ({ date, title, price: exPrice }) => {
   // 원화 표기법으로 변환
   const formattedPrice = new Intl.NumberFormat('ko-KR').format(exPrice);
 
+  // 이벤트 핸들러 선언
+  const clickHandler = e => {
+    console.log('클릭함!');
+    console.log(e.target.previousElementSibling.firstElementChild.textContent);
+    
+    // const $price = document.querySelectorAll('.expense-item__price');
+    // console.log($price);
+  };
+
+
   return (
       <Card className='expense-item'>
         <ExpenseDate exDate={date} />
@@ -38,6 +48,8 @@ const ExpenseItem = ({ date, title, price: exPrice }) => {
           <h2>{title}</h2>
           <div className='expense-item__price'>{formattedPrice}원</div>
         </div>
+        <button id='btn' onClick={clickHandler}>버튼1</button>
+        <button id='btn2' onMouseOver={e => alert('하하!')}>버튼2</button>
       </Card>
   )
 }
