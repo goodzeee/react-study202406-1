e7b1547bdb215daca7a28bd4609972eb3dcf3828
soongu <naong526@naver.com>
2024-06-13 11:43:34 +0900

    # 상향식 데이터 전달

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import ExpenseList from './components/expenses/ExpenseList';
 import NewExpense from './components/new-expense/NewExpense';
 
-import CheckBoxStyle from './components/practice/CheckBoxStyle';
-
 const App = () => {
 
   // 서버에서 지출항목 JSON 배열을 응답받음
@@ -26,10 +24,18 @@ const App = () => {
     },
   ];
 
+
+  // ExpenseForm에게 내려보낼 함수
+  const onAddExpense = (userInput) => {
+    console.log('App.js 가 내려보낸 함수 호출!');
+    // console.log(userInput);
+    expenses.push(userInput);
+    console.log(expenses);
+  };
+
   return (
     <>
-      <CheckBoxStyle />
-      <NewExpense />
+      <NewExpense onSave={onAddExpense} />
       <ExpenseList expenses={expenses} />
     </>
   );
--- src/components/new-expense/ExpenseForm.js
+++ src/components/new-expense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ onAdd }) => {
 
   // 입력칸에 있는 3개의 값을 각각의 상태값으로 관리
   // const [title, setTitle] = useState('');
@@ -68,6 +68,10 @@ const ExpenseForm = () => {
     // };
 
     console.log(userInput);
+
+    // App.js에게 받은 함수를 호출
+    onAdd(userInput);
+
 
     // form input 비우기
     setUserInput({
