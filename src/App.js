136b6ee9caa79269c4a9363716baa813a0985c8b
soongu <naong526@naver.com>
2024-06-17 12:43:22 +0900

    # useRef로 DOM조작하기

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ const App = () => {
   // 회원들이 저장될 배열
   const [userList, setUserList] = useState([]);
 
-  const count = useRef(1);
-  console.log('count: ', count);
-
   const addUserHandler = user => {
-
-    count.current++;
-    console.log('count.current: ', count.current);
 
     console.log(user);
     setUserList(prev => [
